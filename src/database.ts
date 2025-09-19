bb3472705785496f3bdc2610e70d51a31eb833e5
Marek Zelinka <mzelinka17@gmail.com>
2025-09-19 12:06:52 +0200

    add database

---
+++ src/database.ts
@@ -0,0 +1,26 @@
+import fs from "node:fs/promises";
+import type { Database, NoteEntry } from "./types.ts";
+
+const DB_PATH = new URL("../db.json", import.meta.url).pathname;
+
+export async function getDatabase(): Promise<Database> {
+	const rawDatabase = await fs.readFile(DB_PATH, "utf-8");
+
+	return JSON.parse(rawDatabase);
+}
+
+export async function saveDatabase(database: Database): Promise<Database> {
+	const rawDatabase = JSON.stringify(database, null, 2);
+	await fs.writeFile(DB_PATH, rawDatabase);
+
+	return database;
+}
+
+export async function insertData(note: NoteEntry): Promise<NoteEntry> {
+	const database = await getDatabase();
+	database.notes.push(note);
+
+	await saveDatabase(database);
+
+	return note;
+}
